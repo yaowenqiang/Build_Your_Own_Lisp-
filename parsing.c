43579b88ab8b209c0bb7d2a7d8c8f483e71c31dc
Yaowenqiang <yaowenqiang111@163.com>
2021-07-01 21:56:36 +0800

    add eval

--- parsing.c
+++ parsing.c
@@ -5,6 +5,33 @@
 #include <editline/readline.h>
 #include <editline/history.h>
 
+long eval_op(long x, char* op, long y) {
+    if(strcmp(op, "+") == 0) {return x + y;}
+    if(strcmp(op, "-") == 0) {return x - y;}
+    if(strcmp(op, "*") == 0) {return x * y;}
+    if(strcmp(op, "/") == 0) {return x / y;}
+    return 0;
+}
+
+long eval(mpc_ast_t* t) {
+    if (strstr(t->tag, "number")) {
+        return atoi(t->contents);
+    }
+
+    char* op = t->children[1]->contents;
+
+    long x = eval(t->children[2]);
+
+    int i = 3;
+    while(strstr(t->children[i]->tag, "expr")) {
+        x = eval_op(x, op, eval(t->children[i]));
+        i++;
+    }
+    return x;
+
+}
+
+
 int main(int argc, char ** argv) {
     /* Create Some Parsers */
     mpc_parser_t* Number = mpc_new("number");
@@ -29,6 +56,18 @@ int main(int argc, char ** argv) {
 
         if(mpc_parse("<stdin>", input, Lispy, &r)) {
             mpc_ast_print(r.output);
+            mpc_ast_t* a = r.output;
+            printf("Tag: %s\n", a->tag);
+            printf("contents: %s\n", a->contents);
+            printf("Number of childrens:: %i\n", a->children_num);
+
+            mpc_ast_t* c0 = a->children[0];
+            printf("first Child tag: %s\n", c0->tag);
+            printf("first Child Contents:: %s\n", c0->contents);
+            printf("first Child Number of children: %i\n", c0->children_num);
+            long result = eval(r.output);
+            printf("%li\n", result);
+            
             mpc_ast_delete(r.output);
         } else {
             mpc_err_print(r.error);
